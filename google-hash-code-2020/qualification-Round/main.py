219608f5b3c8c7f8a409659485ee652d2bc31814
prime <francis.neequaye@gmail.com>
2022-12-21 22:49:36 +0000

    Restructuring dir.

--- google-hash-code-2020/qualification-Round/main.py
+++ google-hash-code-2020/qualification-Round/main.py
@@ -1,4 +1,6 @@
 """
+Author: francis.neequaye@gmail.com
+
 This program responds to Google Hash Code 2020 conception, qualifying round :
 https://codingcompetitions.withgoogle.com/hashcode/archive.
 This is meant for demonstration purposes, and may be freely used, and adapted.
@@ -9,7 +11,6 @@ def main():
 
     run = BookScanning()
     run.submission_file()
-
 
 if __name__ == "__main__":
     main()
